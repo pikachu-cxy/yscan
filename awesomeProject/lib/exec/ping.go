44616978f7af6aaae67920b2d124a2deab70c185
Pikachu <32427591+pikachu-cxy@users.noreply.github.com>
2023-05-20 15:19:07 +0800

    Update ping.go

--- awesomeProject/lib/exec/ping.go
+++ awesomeProject/lib/exec/ping.go
@@ -1,18 +1,36 @@
 package exec
 
 import (
+	"awesomeProject/lib/Format"
 	"fmt"
 	"os/exec"
 )
 
 func IpIcmp(ip *string) {
 
-	cmd := exec.Command("ping", *ip, "-n", "1", "-w", "200") // Windows系统上使用 "-n" 参数指定发送的 ICMP 请求次数
-	err := cmd.Run()
-	if err != nil {
-		fmt.Printf("执行命令出错：%v\n", err)
-		return
-	} else {
-		fmt.Printf("%s is alive！", *ip)
+	ips, _ := sometone(*ip)
+	if ips != nil {
+		for _, v := range ips {
+			//fmt.Printf("%d and %s",i,v)
+			cmd := exec.Command("ping", v, "-n", "1", "-w", "200") // Windows系统上使用 "-n" 参数指定发送的 ICMP 请求次数
+			err := cmd.Run()
+			if err != nil {
+				fmt.Printf("执行命令出错：%v\n", err)
+				return
+			} else {
+				fmt.Printf("%s is alive！", *ip)
+			}
+
+		}
 	}
+
+}
+
+func sometone(ip string) (s []string, bool2 bool) {
+	ips := Format.ChooseFormat(ip)
+
+	if ips == nil {
+		return nil, false
+	}
+	return ips, true
 }
