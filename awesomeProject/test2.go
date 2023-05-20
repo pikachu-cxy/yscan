fe4714ee8e73015be74eb227902ca8d57426febe
Pikachu <32427591+pikachu-cxy@users.noreply.github.com>
2023-05-20 15:18:36 +0800

    Update test2.go

--- awesomeProject/test2.go
+++ awesomeProject/test2.go
@@ -20,8 +20,9 @@ func main() {
 
 	fmt.Print(logo)
 	// 定义命令行参数
-	ip := flag.String("ip", "127.0.0.1", "IP 地址")
+	ip := flag.String("ip", "127.0.0.1", "输入IP 地址，格式支持：192.168.21.1/24, 192.168.21.1-255, 192.168.21.1-192.168.21.255")
 	flag.Parse()
+
 	//port := flag.Int("port", 0, "端口号")
 	//verbose := flag.Bool("verbose", false, "是否启用详细模式")
 	exec.IpIcmp(ip)
